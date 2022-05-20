fe025aac0b7de83b9a0eec733aa043ad04b1c36c
nepriscila <neyleia16@hotmail.com>
2022-05-19 23:57:49 -0300

    salvando modificação em arquivo js

--- tarefa-dia-3/pratica-1.js
+++ tarefa-dia-3/pratica-1.js
@@ -1,4 +1,4 @@
-let nome = "Neyleia Priscila"
+let nome = "Neyleia"
 let email = "neyleia16@hotmail.com"
 let profissão = "administradora"
 let idade = 28
@@ -9,5 +9,3 @@ console.log(email)
 console.log(profissão)
 console.log(idade)
 console.log(lugar)
-
-Olá, console.log(nome)! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para console.log(lugar). Confirma pra gente alguns dados? A sua idade é console.log(idade), você é console.log(profissão) e o seu email é console.log(email)? Ficamos no aguardo. Muito obrigado e boa viagem! :D
