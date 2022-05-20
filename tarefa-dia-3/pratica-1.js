f8415a4f1c0229ac759eabcb310a038b3feb5897
nepriscila <neyleia16@hotmail.com>
2022-05-19 21:23:49 -0300

    criação de pasta tarefa-dia-3 e adicionado arquivos

---
+++ tarefa-dia-3/pratica-1.js
@@ -0,0 +1,13 @@
+let nome = "Neyleia Priscila"
+let email = "neyleia16@hotmail.com"
+let profissão = "administradora"
+let idade = 28
+let lugar = "Nova York"
+
+console.log(nome)
+console.log(email)
+console.log(profissão)
+console.log(idade)
+console.log(lugar)
+
+Olá, console.log(nome)! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para console.log(lugar). Confirma pra gente alguns dados? A sua idade é console.log(idade), você é console.log(profissão) e o seu email é console.log(email)? Ficamos no aguardo. Muito obrigado e boa viagem! :D
